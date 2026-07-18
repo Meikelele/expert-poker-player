3e908356f31ac17d745a04bb0ddb097a62712ea8
Michał Bąk <m.bak@avsystem.com>
2026-07-18 12:02:53 +0200

    feat(hands): implement hand rank and hand value

---
+++ src/expert_poker_player/hands/__init__.py
@@ -0,0 +1,7 @@
+from expert_poker_player.hands.hand_rank import HandRank
+from expert_poker_player.hands.hand_value import HandValue
+
+__all__ = [
+    "HandRank",
+    "HandValue",
+]
---
+++ src/expert_poker_player/hands/hand_rank.py
@@ -0,0 +1,15 @@
+# uklady rąk przechowujefrom enum import IntEnum
+from enum import IntEnum
+
+class HandRank(IntEnum):
+    """Kategorie układów pokerowych uporządkowane według siły."""
+
+    HIGH_CARD = 0
+    ONE_PAIR = 1
+    TWO_PAIR = 2
+    THREE_OF_A_KIND = 3
+    STRAIGHT = 4
+    FLUSH = 5
+    FULL_HOUSE = 6
+    FOUR_OF_A_KIND = 7
+    STRAIGHT_FLUSH = 8
