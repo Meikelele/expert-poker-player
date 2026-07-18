ff1e67341cc14053593555a8b7a1d3f1dbd56a44
Michał Bąk <m.bak@avsystem.com>
2026-07-18 12:25:36 +0200

    feat(hands): evaluate five-card poker hands

--- src/expert_poker_player/hands/__init__.py
+++ src/expert_poker_player/hands/__init__.py
@@ -1,7 +1,9 @@
+from expert_poker_player.hands.evaluator import evaluate_five_card_hand
 from expert_poker_player.hands.hand_rank import HandRank
 from expert_poker_player.hands.hand_value import HandValue
 
 __all__ = [
     "HandRank",
     "HandValue",
+    "evaluate_five_card_hand",
 ]
